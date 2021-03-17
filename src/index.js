b7d51ac5d3cbfd38b22ed45a14df183940d81906
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-17 00:59:40 -0700

    d

--- src/index.js
+++ src/index.js
@@ -89,6 +89,14 @@ class Game extends React.Component {
   }
 }
 
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -108,10 +116,3 @@ function calculateWinner(squares) {
   return null;
   }
 }
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
