72f5e01d765474a32a8a39e2092a6ea6123bae2a
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-17 23:42:57 -0700

    d

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ class Game extends React.Component {
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: (step % 2) === 0
     });
   }
 
@@ -131,8 +131,8 @@ function calculateWinner(squares) {
     [3, 4, 5],
     [6, 7, 8],
     [0, 4, 8],
-    [1, 4, 7],
     [2, 4, 6],
+    [1, 4, 7],
     [0, 3, 6],
     [2, 5, 8],
   ];
