3433eae101a05dd9b805d3c5d02a59da1e10703a
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-18 21:40:47 -0700

    u

--- src/index copy.js
+++ src/index copy.js
@@ -141,6 +141,6 @@ function calculateWinner(squares) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-  return null;
   }
+  return null;
 }
